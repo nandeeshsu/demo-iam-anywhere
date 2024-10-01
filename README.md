c3eb1f4f30cc0297a72ae25a4084abb5c7d3d0ea
Nandeesh Subbegowda <subbegowda@gmail.com>
2024-10-02 00:49:16 +0100

    updated openssl.conf

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+Refered links
+
 https://dev.to/polarsquad/how-to-use-aws-roles-anywhere-484p
+
 https://aws.amazon.com/blogs/security/iam-roles-anywhere-with-an-external-certificate-authority/
+
 https://aws.amazon.com/blogs/security/extend-aws-iam-roles-to-workloads-outside-of-aws-with-iam-roles-anywhere/
+
 https://sidhurana.medium.com/why-aws-iam-roles-anywhere-and-how-to-use-it-from-on-premises-with-best-security-practices-b1213aefb684
--- cert/generatekey.sh
+++ cert/generatekey.sh
@@ -3,13 +3,13 @@
 #Create CA private key
 openssl genrsa -out MyCA.key 4096
 
-#Create CA certificate
+#Create CA certificate with above generated private key
 openssl req -new -x509 -days 3650 -key MyCA.key -out MyCA.pem -extensions v3_ca -config ./openssl.cnf
 
-#Create client key
+#Create client Private key for On-Premises VM. Specific to client machine who wants to access to AWS
 openssl genrsa -out client.key 4096
 
-#Create client CSR
+#Create client CSR using the above generated client private key - Certificate Signing Request for On-Premises VM who wants to access AWS
 openssl req -new -key client.key -out client.csr -config ./openssl.cnf
 
 #Client version 3 extension config
@@ -19,5 +19,5 @@ authorityKeyIdentifier = keyid,issuer
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment, dataEncipherment
 EOF
 
-#Create Client certificate
+#Create Client certificate - end entity certificate for a specific on-premises vm/server
 openssl x509 -req -in client.csr -CA MyCA.pem -CAkey MyCA.key -CAcreateserial -out client.pem -days 3650 -sha256 -extfile client.ext
