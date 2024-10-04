1e8b77b1bac066c15b334040d4d6c6b21b31c3e6
Nandeesh Subbegowda <subbegowda@gmail.com>
2024-10-05 00:21:02 +0100

    Update generatekey.sh

--- cert/generatekey.sh
+++ cert/generatekey.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #Create CA private key
-openssl genrsa -out MyCA.key 4096
+openssl genrsa -out MyAWSCA.key 4096
 
 #Create CA certificate with above generated private key
-openssl req -new -x509 -days 3650 -key MyCA.key -out MyCA.pem -extensions v3_ca -config ./openssl.cnf
+openssl req -new -x509 -days 3650 -key MyAWSCA.key -out MyAWSCA.pem -extensions v3_ca -config ./openssl.cnf
 
-#Create client Private key for On-Premises VM. Specific to client machine who wants to access to AWS
-openssl genrsa -out client.key 4096
+#Create onpremise Private key for On-Premises VM. Specific to onpremise machine who wants to access to AWS
+openssl genrsa -out onpremise.key 4096
 
-#Create client CSR using the above generated client private key - Certificate Signing Request for On-Premises VM who wants to access AWS
-openssl req -new -key client.key -out client.csr -config ./openssl.cnf
+#Create onpremise CSR using the above generated onpremise private key - Certificate Signing Request for On-Premises VM who wants to access AWS
+openssl req -new -key onpremise.key -out onpremise.csr -config ./openssl.cnf
 
-#Client version 3 extension config
-cat > client.ext<<EOF
+#onpremise version 3 extension config
+cat > onpremise.ext<<EOF
 basicConstraints = CA:FALSE
 authorityKeyIdentifier = keyid,issuer
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment, dataEncipherment
 EOF
 
-#Create Client certificate - end entity certificate for a specific on-premises vm/server
-openssl x509 -req -in client.csr -CA MyCA.pem -CAkey MyCA.key -CAcreateserial -out client.pem -days 3650 -sha256 -extfile client.ext
+#Create onpremise certificate - end entity certificate for a specific on-premises vm/server
+openssl x509 -req -in onpremise.csr -CA MyAWSCA.pem -CAkey MyAWSCA.key -CAcreateserial -out onpremise.pem -days 3650 -sha256 -extfile onpremise.ext
